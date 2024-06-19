48e47bed0a70412ae6fc7c7c01fc911be582728f
Ayush Hang Verma <ayush.hang25@gmail.com>
2024-06-20 02:56:51 +0530

    Add files via upload

--- BPP.py
+++ BPP.py
@@ -18,55 +18,64 @@ Pattern Progs:
 '''
 
 #-------------------------------------------------------------------------------------------------
-#program vvv
-
+#importing libraries in the program vvv
 
 import time
-
 
 
 #-------------------------------------------------------------------------------------------------
 #this is the pattern programs section vvv
 
 def pattern_programs():
-    print("You are in the Pattern Programs Menu")
-    while True:
-        print("Press 1 for Triangles \nPress 2 for Box Box Box \nPress 3 for Circle \nPress 4 to Exit")
-        chc1=input("Enter your choice :")
-        if chc1==1:
-            triangle_menu()
-        elif chc==2:
-            box_menu()
-        elif chc==3:
-            circle_menu()
+    print("\nYou are in the Pattern Programs Menu")
+    print("Press 1 for Triangles \nPress 2 for Box Box Box \nPress 3 for Circle \nPress 4 to go back to the Main Menu \n")
+    chc1=input("Enter your choice :")
+    if chc1=="1":
+        triangle_menu()
+    elif chc1=="2":
+        box_menu()
+    elif chc1=="3":
+        circle_menu()
+    elif chc1=="4":
+        print("You will be transfered to the main menu :) ")
+        time.sleep(0.5)
+        main_menu()
+    else:
+        print("There must have been some error")
+
 
 
 def triangle_menu():
-    print("Triangle Menu :\n1. [Left Side]Right Angle Icosceles Triangle \n2. [Right Side]Right Angle Icosceles Triangle \n3. [Center]Equilateral Triangle \n4. [Left Side]Equilateral Triangle \n5. [Right Side]Equilateral Triangle ")
+    print("\nTriangle Menu :\n1. [Left Side]Right Angle Icosceles Triangle \n2. [Right Side]Right Angle Icosceles Triangle \n3. [Center]Equilateral Triangle \n4. [Left Side]Equilateral Triangle \n5. [Right Side]Equilateral Triangle ")
 
 def box_menu():
-    print("Box Menu :\n1. Square \n2. Rectangle \n3. Diamond")
+    print("\nBox Menu :\n1. Square \n2. Rectangle \n3. Diamond")
 
 def circle_menu():
-    print("Circle Menu :\n1. Simple Circle \n2. Semi Circle \n3. Spinning Donut")
+    print("\nCircle Menu :\n1. Simple Circle \n2. Semi Circle \n3. Spinning Donut")
 
 
 #-------------------------------------------------------------------------------------------------
 #this is the basic maths program section vvv
 
 def basic_math_prog():
-    print("You are in the basic mathematics programs menu ")
-    while True:
-        print("Press 1 for Addition Programs \nPress 2 for Subtraction Programs \nPress 3 for Multiplication Programs \n Press 4 for Division Programs")
-        chc2=input("Enter your choice :")
-        if chc2==1:
-            addition_programs()
-        elif chc2==2:
-            subtraction_programs()
-        elif chc2==3:
-            multiplication_progarm()
-        elif chc2==4:
-            division_programs()
+    print("\nYou are in the basic mathematics programs menu ")
+    print("\nPress 1 for Addition Programs \nPress 2 for Subtraction Programs \nPress 3 for Multiplication Programs \nPress 4 for Division Programs \nPress 5 to Go back to the Main Menu \n")
+    chc2=input("Enter your choice :")
+    if chc2=="1":
+        addition_programs()
+    elif chc2=="2":
+        subtraction_programs()
+    elif chc2=="3":
+        multiplication_progarm()
+    elif chc2=="4":
+        division_programs()
+    elif chc2=="5":
+        print("You will be transfered to the main menu :) ")
+        time.sleep(0.5) #time delay so that everything doesnt happen in a flash (keep it at 0.5)
+        main_menu()
+    else:
+        print("There must have been some error")
         
 
 def addition_programs():
@@ -80,6 +89,7 @@ def addition_programs():
             sum=sum+value
             numbers_to_add=numbers_to_add-1
         print("The total sum is:",sum)
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
         print("Here is the code for the above operation :")
         print('''
     numbers_to_add=int(input("How many numbers do you want to add: "))
@@ -93,9 +103,14 @@ def addition_programs():
         print("The total sum is:",sum)
     else:
         print("How can you add "+str(numbers_to_add)+" numbers ")''')
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        basic_math_prog() #function recall to take you back to the basic maths menu
         
     else:
         print("How can you add "+str(numbers_to_add)+" numbers ")
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        basic_math_prog() #function recall to take you back to the basic maths menu
+
 
 def subtraction_programs():
     print("Hi! welcome to the Subtraction Section ")
@@ -103,12 +118,16 @@ def subtraction_programs():
     sub2=float(input("Enter the second number: "))
     sub=sub1-sub2
     print("The difference between the two is: ",sub)
+    time.sleep(1) #time delay so that everything doesnt happen in a flash
     print("Here is the code for the above operation :")
     print('''
     sub1=float(input("Enter the first number: "))
     sub2=float(input("Enter the second number: ")) 
     sub=sub1-sub2 
     print("The difference between the two is: ",sub)''')
+    time.sleep(1) #time delay so that everything doesnt happen in a flash
+    basic_math_prog() #function recall to take you back to the basic maths menu
+
 
 def multiplication_progarm():
     print("Hi! welcome to the Multiplication Section ")
@@ -121,6 +140,7 @@ def multiplication_progarm():
             sum=sum*value
             numbers_to_mult=numbers_to_mult-1
         print("The total after multiplying everything is:",sum)
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
         print("Here is the code for the above operation :")
         print('''
     numbers_to_mult=int(input("How many numbers do you want to multiply: "))
@@ -135,51 +155,124 @@ def multiplication_progarm():
     else:
         print("How can you multiply "+str(numbers_to_mult)+" numbers ")
               ''')
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        basic_math_prog() #function recall to take you back to the basic maths menu
     else:
         print("How can you multiply "+str(numbers_to_mult)+" numbers ")
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        basic_math_prog() #function recall to take you back to the basic maths menu
+
 
 def division_programs():
-    print("Press 1 to perform a simple division between two numbers \nPress 2 to perform a reccuring division with each answer \nPress 3 to terminate")
-    chc=("Enter your choice : ")
-    while True:
-        if chc==1:
-            num1=float(input("Enter the number you want to divide"))
-            num2=float(input("Enter the number you want to dive with"))
-            val=num1/num2
-            print("The quotient is : ",val)
-            rem=num1%num2
-            print("The remainder is : ",rem)
-        elif chc==2:
-            num1=float(input("Enter the number you want to start dividing with :"))
-            decision=True
-            while decision==True:
-                num2=float(input("Enter the number you want to divide with"))
-                #try to make the naswer come out and based on the users reply whather they want to conitnue dividing or not
-                #take the input nad do it again 
+    print("Press 1 to perform a simple division between two numbers \nPress 2 to perform a reccuring division with each answer ")
+    chc=input("Enter your choice : ")
+    if chc=="1":
+        num1=float(input("Enter the number you want to divide : "))
+        num2=float(input("Enter the number you want to dive with : "))
+        val=num1/num2
+        print("The quotient is : ",val)
+        rem=num1%num2
+        print("The remainder is : ",rem)
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        print("Here is the code for the above operation")
+        print('''
+        num1=float(input("Enter the number you want to divide : "))
+        num2=float(input("Enter the number you want to dive with : "))
+        val=num1/num2
+        print("The quotient is : ",val)
+        rem=num1%num2
+        print("The remainder is : ",rem)
+              ''')
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        basic_math_prog() #function recall to take you back to the basic maths menu
+        return False
+    elif chc=="2":
+        num1=float(input("Enter the number you want to start dividing with :"))
+        while True:
+            num2=float(input("Enter the number you want to divide with :"))
+            div=num1/num2
+            print("The answer is (quotient): ",div)
+            choice=input("Do you wish to divide further (y/n) ?")
+            if choice=="y" or choice=="Y":
+                print("New Dividend : ",div)
+                num1=div
+                num2=0.0
+            else :
+                print("Final Answer : ",div)
+                time.sleep(1) #time delay so that everything doesnt happen in a flash
+                print("Here is the code for the above operation")
+                print('''
+        num1=float(input("Enter the number you want to start dividing with :"))
+        while True:
+            num2=float(input("Enter the number you want to divide with :"))
+            div=num1/num2
+            print("The answer is (quotient): ",div)
+            choice=input("Do you wish to divide further (y/n) ?")
+            if choice=="y" or choice=="Y":
+                print("New Dividend : ",div)
+                num1=div
+                num2=0.0
+            else :
+                print("Final Answer : ",div)
+                return False
+                      ''')
+                time.sleep(1) #time delay so that everything doesnt happen in a flash
+                basic_math_prog() #function recall to take you back to the basic maths menu
+                return False
+    else:
+        print("There must have been some error")
+        time.sleep(1) #time delay so that everything doesnt happen in a flash
+        basic_math_prog() #function recall to take you back to the basic maths menu
                 
 
 
 #-------------------------------------------------------------------------------------------------
 #this is the calculator programs section vvv 
 
-
 def calculator_prog():       
-    print("yooo")
+    print("Under Construction")
+    main_menu()
 
 
 
 
 
 #-------------------------------------------------------------------------------------------------
-#this is the primary menu section vvv
+#this is the animated print section
+
 def anima_print(text):
   for char in text:
     print(char, end='', flush=True)
     time.sleep(0.003)
 
+#-------------------------------------------------------------------------------------------------
+#this is the main menu you see in the start
+
+def main_menu():
+    print("\nEnter the serial numbers of the topics that interest you to access them")
+    print("1. All Pattern Programs \n2. Basic Mathematics Programs \n3. Calculator Program \n4. \n5. Exit")
+    chc=int(input("Enter your choice [1,2,3,4 or 5] : "))
+    if(chc==1):
+        pattern_programs()
+    elif(chc==2):
+        basic_math_prog()
+    elif(chc==3):
+        calculator_prog()
+    elif(chc==4):
+        ()
+    elif(chc==5):
+        anima_print('''
+    |\__/,|   (`)
+  _.|o o  |_   ) )
+-(((---(((-------------------------------------------------------------------------------------------------------------
+                                        ''')
+        print("Thank you for using BPP.")
+    else:
+        print("You must have made a wrong choice, you can always start again :) ")
 
 
-
+#-------------------------------------------------------------------------------------------------
+#this is the function call 
 
 anima_print('''
     |\__/,|   (`)
@@ -194,23 +287,10 @@ anima_print('''
                                                                                                 ~ 𝗮𝘆𝘂𝘀𝗵𝗵𝗮𝗻𝗴
             
 
-                        ~ 𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝚝𝚘 𝚝𝚑𝚎 𝙱𝚊𝚜𝚒𝚌 𝙱𝚎𝚐𝚒𝚗𝚗𝚎𝚛 𝙿𝚛𝚘𝚐𝚛𝚊𝚖𝚜 𝙼𝚎𝚗𝚞 ~ \n\n<> 𝚈𝚘𝚞𝚛 𝚗𝚞𝚖𝚙𝚊𝚍 𝚠𝚒𝚕𝚕 𝚋𝚎 𝚝𝚑𝚎 𝚗𝚊𝚟𝚒𝚐𝚊𝚝𝚘𝚛 𝚝𝚑𝚛𝚘𝚞𝚐𝚑 𝚝𝚑𝚒𝚜 𝚙𝚛𝚘𝚐𝚛𝚊𝚖
-<> 𝚃𝚑𝚒𝚜 𝚒𝚜 𝚊 𝚙𝚛𝚘𝚐𝚛𝚊𝚖 𝚝𝚑𝚊𝚝 𝚌𝚘𝚗𝚝𝚊𝚒𝚗𝚜 𝚊 𝚖𝚊𝚓𝚘𝚛𝚒𝚝𝚢 𝚘𝚏 𝚋𝚊𝚜𝚒𝚌 𝚙𝚛𝚘𝚐𝚛𝚊𝚖𝚜 𝚍𝚘𝚗𝚎 𝚒𝚗 𝚌𝚘𝚕𝚕𝚎𝚐𝚎 \n𝚊𝚗𝚍 s𝚌𝚑𝚘𝚘𝚕 𝚕𝚎𝚟𝚎𝚕, 𝚞𝚜𝚎 𝚒𝚝 𝚠𝚒𝚜𝚎𝚕𝚢 ''')
+                        ~ 𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝚝𝚘 𝚝𝚑𝚎 𝙱𝚊𝚜𝚒𝚌 𝙱𝚎𝚐𝚒𝚗𝚗𝚎𝚛 𝙿𝚛𝚘𝚐𝚛𝚊𝚖𝚜 𝙼𝚎𝚗𝚞 ~ 
+
+<> 𝚈𝚘𝚞𝚛 𝚗𝚞𝚖𝚙𝚊𝚍 𝚠𝚒𝚕𝚕 𝚋𝚎 𝚝𝚑𝚎 𝚗𝚊𝚟𝚒𝚐𝚊𝚝𝚘𝚛 𝚝𝚑𝚛𝚘𝚞𝚐𝚑 𝚝𝚑𝚒𝚜 𝚙𝚛𝚘𝚐𝚛𝚊𝚖
+<> 𝚃𝚑𝚒𝚜 𝚒𝚜 𝚊 𝚙𝚛𝚘𝚐𝚛𝚊𝚖 𝚝𝚑𝚊𝚝 𝚌𝚘𝚗𝚝𝚊𝚒𝚗𝚜 𝚊 𝚖𝚊𝚓𝚘𝚛𝚒𝚝𝚢 𝚘𝚏 𝚋𝚊𝚜𝚒𝚌 𝚙𝚛𝚘𝚐𝚛𝚊𝚖𝚜 𝚍𝚘𝚗𝚎 𝚒𝚗 𝚌𝚘𝚕𝚕𝚎𝚐𝚎 
+   𝚊𝚗𝚍 s𝚌𝚑𝚘𝚘𝚕 𝚕𝚎𝚟𝚎𝚕, 𝚞𝚜𝚎 𝚒𝚝 𝚠𝚒𝚜𝚎𝚕𝚢 ''')
 time.sleep(0.5)
-print("\n\nEnter the serial numbers of the topics that interest you to access them")
-while True:
-    print("1. All Pattern Programs \n2. Basic Mathematics Programs \n3. Calculator Program \n4. \n5. Exit")
-    chc=int(input("Enter your choice[1,2,3,4 or 5]:"))
-    if(chc==1):
-        pattern_programs()
-    elif(chc==2):
-        basic_math_prog()
-    elif(chc==3):
-        calculator_prog()
-    elif(chc==4):
-        ()
-    elif(chc==5):
-        print("Thank you.")
-        break
-    else:
-        print("You must have made a wrong choice, you can always start again :) ")
+main_menu()
